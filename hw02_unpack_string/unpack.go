98238af8cc3d7067ae96a1c5173d8e28edb12751
Artemy Okulov <artemy.okulov@x5.ru>
2024-04-14 12:00:59 +0300

    done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,64 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
+var escapeRune rune = []rune(`\`)[0]
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func getSequenceLen(arr []rune, index int) int {
+	elem := arr[index]
+	result := 1
+	newIndex := index - 1
+	for newIndex > 0 {
+		if arr[newIndex] == elem {
+			result++
+		} else {
+			return result
+		}
+		newIndex--
+	}
+	return result
+}
+
+func isEscaped(arr []rune, index int) bool {
+	if index <= 0 || index >= len(arr) {
+		return false
+	}
+	return (arr[index-1] == escapeRune && getSequenceLen(arr, index-1)%2 != 0)
+}
+
+func isCount(arr []rune, index int) bool {
+	if index < 0 || index >= len(arr) {
+		return false
+	}
+	return unicode.IsDigit(arr[index]) && !isEscaped(arr, index)
+}
+
+func Unpack(input string) (string, error) {
+	var result strings.Builder
+
+	inputAsRuneArr := []rune(input)
+	inputAsRuneArrLen := len(inputAsRuneArr)
+
+	for i, v := range inputAsRuneArr {
+		if isEscaped(inputAsRuneArr, i) && ((i+1 >= inputAsRuneArrLen) || !unicode.IsDigit(inputAsRuneArr[i+1])) {
+			result.WriteRune(v)
+		} else if isCount(inputAsRuneArr, i) {
+			if (i-1 < 0) || (isCount(inputAsRuneArr, i-1)) {
+				return "", ErrInvalidString
+			}
+			literalStr := string(inputAsRuneArr[i-1])
+			digit, _ := strconv.Atoi(string(v))
+			result.WriteString(strings.Repeat(literalStr, digit))
+
+		} else if ((i+1 >= inputAsRuneArrLen) || !unicode.IsDigit(inputAsRuneArr[i+1])) && v != escapeRune {
+			result.WriteRune(v)
+		}
+	}
+
+	return result.String(), nil
 }
