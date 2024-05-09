e0849832decd4ea3a09c942f1c6ab39c3a36b218
Artemy Okulov <artemy.okulov@x5.ru>
2024-05-10 00:22:41 +0300

    done hw4

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,25 +1,72 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
-	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
+	Set(key Key, value interface{}) bool
 	Clear()
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
+	capacity     int
+	m            sync.Mutex
+	queue        List
+	items        map[Key]*ListItem
+	reverseItems map[*ListItem]Key
+}
 
-	capacity int
-	queue    List
-	items    map[Key]*ListItem
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	cache.m.Lock()
+	defer cache.m.Unlock()
+
+	if val, ok := cache.items[key]; ok {
+		val.Value = value
+		cache.queue.MoveToFront(val)
+		return true
+	}
+
+	var newItem = cache.queue.PushFront(value)
+	newItem.Value = value
+	cache.items[key] = newItem
+	cache.reverseItems[newItem] = key
+
+	if cache.queue.Len() > cache.capacity {
+		last := cache.queue.Back()
+		delete(cache.items, cache.reverseItems[last])
+		delete(cache.reverseItems, last)
+		cache.queue.Remove(last)
+	}
+	return false
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	cache.m.Lock()
+	defer cache.m.Unlock()
+
+	if value, ok := cache.items[key]; ok {
+		cache.queue.MoveToFront(value)
+		return value.Value, true
+	}
+
+	return nil, false
+}
+
+func (cache *lruCache) Clear() {
+	cache.m.Lock()
+	defer cache.m.Unlock()
+	cache.items = make(map[Key]*ListItem)
+	cache.reverseItems = make(map[*ListItem]Key)
+	cache.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
-		capacity: capacity,
-		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		capacity:     capacity,
+		queue:        NewList(),
+		items:        make(map[Key]*ListItem, capacity),
+		reverseItems: make(map[*ListItem]Key),
 	}
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,13 +50,49 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(3)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+		c.Set("ddd", 400)
+
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
+
+		val, ok := c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		val, ok = c.Get("ccc")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
+
+		c.Get("bbb")
+		c.Set("bbb", 999)
+		c.Set("eee", 500)
+
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 999, val)
+
+		val, ok = c.Get("eee")
+		require.True(t, ok)
+		require.Equal(t, 500, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
 	})
 }
 
 func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
-
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
