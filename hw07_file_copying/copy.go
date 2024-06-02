21c4c0d16dbe1368d66d2af2d5dfa828452cc185
Artemy Okulov <artemy.okulov@x5.ru>
2024-06-02 22:19:22 +0300

    done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,95 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrUnsupportedFile       = errors.New("unsupported input file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSameFiles             = errors.New("input and output are same files")
 )
 
+func getFileSize(filepath string) int64 {
+	fileInfo, err := os.Lstat(filepath)
+	if err != nil {
+		return 0
+	}
+	return fileInfo.Size()
+}
+
+func validateInput(fromPath, toPath string, offset int64) error {
+	if file, err := os.OpenFile(fromPath, os.O_RDONLY, 0); err != nil {
+		return fmt.Errorf("can not read input file: %w", err)
+	} else {
+		if err := file.Close(); err != nil {
+			return fmt.Errorf("check file read: close input file error: %w", err)
+		}
+	}
+	inputFileSize := getFileSize(fromPath)
+	if inputFileSize == 0 {
+		return ErrUnsupportedFile
+	}
+	if fromPath == toPath {
+		return ErrSameFiles
+	}
+	if offset >= inputFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+	return nil
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if err := validateInput(fromPath, toPath, offset); err != nil {
+		return err
+	}
+	inputFileSize := getFileSize(fromPath)
+	if limit > inputFileSize || limit == 0 {
+		limit = inputFileSize - offset
+	}
+	inputFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return fmt.Errorf("open input file error: %w", err)
+	}
+	if offset > 0 {
+		inputFile.Seek(offset, 0)
+	}
+	outputFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("output file error: %w", err)
+	}
+
+	defer func() {
+		inputFile.Close()
+	}()
+	defer func() {
+		outputFile.Close()
+	}()
+
+	readPartSize := int64(1024)
+	bar := pb.Start64(limit)
+	buffer := make([]byte, readPartSize)
+
+	for limit > 0 {
+		readedBytesCount, err := inputFile.Read(buffer[:min(readPartSize, limit)])
+		if err != nil && err != io.EOF {
+			return fmt.Errorf("error while reading from input file: %w", err)
+		}
+		if err == io.EOF && readedBytesCount == 0 {
+			break
+		}
+		_, err = outputFile.Write(buffer[:readedBytesCount])
+		if err != nil && err != io.EOF {
+			return fmt.Errorf("error while writing to output file: %w", err)
+		}
+		limit = limit - int64(readedBytesCount)
+		bar.Add64(int64(readedBytesCount))
+	}
+
+	bar.Finish()
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,48 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func fileAreEquals(filePath1, filePath2 string) bool {
+	f1, _ := os.ReadFile(filePath1)
+	f2, _ := os.ReadFile(filePath2)
+	return bytes.Equal(f1, f2)
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+
+	t.Run("success copy", func(t *testing.T) {
+		outFile, err := os.CreateTemp("", "*")
+		require.NoError(t, err)
+		inputFilePath := "./testdata/input.txt"
+		Copy(inputFilePath, outFile.Name(), 0, 0)
+		require.True(t, fileAreEquals(inputFilePath, outFile.Name()))
+	})
+
+	t.Run("fail if offset > file size", func(t *testing.T) {
+		outFile, err := os.CreateTemp("", "*")
+		require.NoError(t, err)
+		inputFilePath := "./testdata/input.txt"
+		err = Copy(inputFilePath, outFile.Name(), getFileSize(inputFilePath)*2, 0)
+		require.Equal(t, ErrOffsetExceedsFileSize, err)
+	})
+
+	t.Run("/dev/urandom as input raise error", func(t *testing.T) {
+		outFile, err := os.CreateTemp("", "*")
+		require.NoError(t, err)
+		inputFilePath := "/dev/urandom"
+		err = Copy(inputFilePath, outFile.Name(), 0, 0)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("from-to same file", func(t *testing.T) {
+		inputFilePath := "./testdata/input.txt"
+		err := Copy(inputFilePath, inputFilePath, 0, 0)
+		require.Equal(t, ErrSameFiles, err)
+	})
 }
