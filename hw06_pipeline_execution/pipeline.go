5aec79a06fe568d860f40e4a33379ea43ade9d7d
Artemy Okulov <artemy.okulov@x5.ru>
2024-06-06 21:47:22 +0300

    fix

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -18,12 +18,12 @@ type Task struct {
 	result interface{}
 }
 
-func execChain(elem interface{}, done In, stages ...Stage) interface{} {
+func execChain(elem interface{}, done In, stages ...Stage) (result interface{}, ok bool) {
 	ch := make(Bi, 1)
 	ch <- elem
 	close(ch)
 	if len(stages) < 1 {
-		return elem
+		return elem, true
 	}
 	processedValue, ok := <-(stages[0](ch))
 	if !ok {
@@ -31,10 +31,10 @@ func execChain(elem interface{}, done In, stages ...Stage) interface{} {
 	}
 	select {
 	case <-done:
-		return nil
+		return nil, false
 	default:
 		if len(stages) == 1 {
-			return processedValue
+			return processedValue, true
 		}
 		return execChain(processedValue, done, stages[1:]...)
 	}
@@ -62,15 +62,19 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 				counter++
 				go func(elem interface{}, id int) {
 					defer wg.Done()
-					task := Task{
-						id:     id,
-						result: execChain(elem, done, stages...),
-					}
 					select {
 					case <-done:
 						return
 					default:
-						outCh <- task
+						if result, ok := execChain(elem, done, stages...); ok {
+							task := Task{
+								id:     id,
+								result: result,
+							}
+							outCh <- task
+						} else {
+							return
+						}
 					}
 				}(elem, counter)
 			}
