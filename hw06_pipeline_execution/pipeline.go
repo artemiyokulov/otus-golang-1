80532cd99b68cfa63253125a90fb2e67fcee99ef
Artemy Okulov <artemy.okulov@x5.ru>
2024-06-06 21:32:00 +0300

    uff, done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,10 @@
 package hw06pipelineexecution
 
+import (
+	"log"
+	"sync"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,7 +13,69 @@ type (
 
 type Stage func(in In) (out Out)
 
+type Task struct {
+	id     int
+	result interface{}
+}
+
+func execChain(elem interface{}, done In, stages ...Stage) interface{} {
+	ch := make(Bi, 1)
+	ch <- elem
+	close(ch)
+	if len(stages) < 1 {
+		return elem
+	}
+	processedValue, ok := <-(stages[0](ch))
+	if !ok {
+		log.Fatal("corrupted stage")
+	}
+	select {
+	case <-done:
+		return nil
+	default:
+		if len(stages) == 1 {
+			return processedValue
+		}
+		return execChain(processedValue, done, stages[1:]...)
+	}
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	outCh := make(Bi)
+	go func() {
+		wg := sync.WaitGroup{}
+		counter := 0
+		defer func() {
+			wg.Wait()
+			close(outCh)
+		}()
+		for {
+			select {
+			case <-done:
+				wg.Add(-counter)
+				return
+			case elem, ok := <-in:
+				if !ok {
+					return
+				}
+				wg.Add(1)
+				counter++
+				go func(elem interface{}, id int) {
+					defer wg.Done()
+					task := Task{
+						id:     id,
+						result: execChain(elem, done, stages...),
+					}
+					select {
+					case <-done:
+						return
+					default:
+						outCh <- task
+					}
+				}(elem, counter)
+			}
+		}
+
+	}()
+	return outCh
 }
